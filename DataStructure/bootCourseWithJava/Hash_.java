5cc0d7e8466b63b8e49cc8f1fe5230cb9b8a7442
hyoin <kslove1111@gmail.com>
2024-02-14 22:18:27 +0900

    자료구조 - 해시 생성자 추가

--- DataStructure/bootCourseWithJava/Hash_.java
+++ DataStructure/bootCourseWithJava/Hash_.java
@@ -19,4 +19,15 @@ public class Hash_ {
     int numElements, tableSize;
     double maxLoadFactor;
     LinkedList_<HashElement<K, V>>[] harray;
+
+    public Hash_(int tableSize) {
+        this.tableSize = tableSize;
+        harray = (LinkedList_<HashElement<K,V>>[]) new LinkedList_[tableSize];
+        // 연결 리스트 체이닝
+        for (int i = 0; i < tableSize; i++) {
+            harray[i] = new LinkedList_<HashElement<K, V>>();
+        }
+        maxLoadFactor = 0.75;
+        numElements = 0;
+    }
 }
