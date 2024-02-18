9e5c50212069a17c321696b14dabc5393fd305e5
hyoin <kslove1111@gmail.com>
2024-02-18 15:38:35 +0900

    자료구조 - 해시 add(), remove()

--- DataStructure/bootCourseWithJava/Hash_.java
+++ DataStructure/bootCourseWithJava/Hash_.java
@@ -1,5 +1,7 @@
 package DataStructure.bootCourseWithJava;
 
+import java.util.LinkedList;
+
 public class Hash_ {
     class HashElement<K, V> implements Comparable<HashElement<K, V>> {
         K key;
@@ -18,16 +20,53 @@ public class Hash_ {
 
     int numElements, tableSize;
     double maxLoadFactor;
-    LinkedList_<HashElement<K, V>>[] harray;
+    LinkedList<HashElement<K, V>>[] harray;
 
     public Hash_(int tableSize) {
         this.tableSize = tableSize;
-        harray = (LinkedList_<HashElement<K,V>>[]) new LinkedList_[tableSize];
+        harray = (LinkedList<HashElement<K,V>>[]) new LinkedList[tableSize];
         // 연결 리스트 체이닝
         for (int i = 0; i < tableSize; i++) {
-            harray[i] = new LinkedList_<HashElement<K, V>>();
+            harray[i] = new LinkedList<HashElement<K, V>>();
         }
         maxLoadFactor = 0.75;
         numElements = 0;
+    }
+
+    public boolean add(K key, V value) {
+        if (loadFactor() > maxLoadFactor) {
+            resize(tableSize * 2);
+        }
+
+        HashElement<K, V> he = new HashElement<K, V>(key, value);
+
+        int hashval = key.hashCode();
+        hashval = hashval & 0x7FFFFFFF;
+        hashval = hashval % tableSize;
+
+        harray[hashval].add(he);
+        numElements++;
+
+        return true;
+    }
+
+    public boolean remove(K key, V value) {
+        HashElement<K, V> = new HashElement<K, V>(key, value);
+
+        int hashval = key.hashCode();
+        hashval = hashval & 0x7FFFFFFF;
+        hashval = hashval % tableSize;
+
+        harray[hashval].remove(he);
+        numElements--;
+
+        return true;
+    }
+
+    public double loadFactor() {
+        if (numElements == 0) {
+            return 0;
+        }
+        return numElements / tableSize;
     }
 }
